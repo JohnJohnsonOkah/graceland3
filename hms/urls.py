c0289c2469125e9129bbb4eb7e4af011af65709d
JAY <johnjohnsonokah@gmail.com>
2019-01-08 15:18:03 +0100

    Setup url

--- hms/urls.py
+++ hms/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('hms.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.dashboard, name='dashboard'),
+    path('', views.reservation, name='reservation'),
+    path('', views.restandbar, name='restandbar'),
+    path('', views.invetory, name='inventory'),
+    path('', views.room, name='room'),    
 ]
