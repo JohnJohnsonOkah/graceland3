3c3843c2b0d0800df360544ebe872db55255beb8
JAY <johnjohnsonokah@gmail.com>
2019-01-08 15:26:55 +0100

    Setup hms views and template

--- hms/views.py
+++ hms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def dashboard(request):
+    return render(request, 'hms/dashboard.html')
+
+def reservation(request):
+    return render(request, 'hms/reservation.html')
+
+def restandbar(request):
+    return render(request, 'hms/restandbar.html')
+
+def inventory(request):
+    return render(request, 'hms/inventory.html')
+
+def room(request):
+    return render(request, 'hms/room.html')
